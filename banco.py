2597c9434e432929fc9d94d183d70a7d81c68675
claraks <claraklassmann@gmail.com>
2025-08-16 14:59:48 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,1 +1,25 @@
+contas = []
 
+def criar_conta():
+  print("\n--- Criar Conta ---")
+  nome = input("Nome Completo: ")
+  numero = input("Número da Conta: ")
+  senha = input("Senha: ")
+  saldo = float(input("Depósito inicial: R$ ")
+
+  conta = {"nome": nome, "numero": numero, "senha": senha, "saldo": saldo}
+  contas.append(conta)
+  print(f"Conta criada com sucesso {nome}!\n")
+  
+def login():
+  print("\n--- Login ---")
+  numero = input ("Número da conta: ")
+  senha = input("Senha: ")
+
+  for conta in contas:
+    if conta["numero] == numero and conta["senha"]== senha:
+        print(f"\n  Bem-vindo(a), {conta['nome']}!\n)
+        return conta
+  print("Conta ou senha inválida. \n")
+  return None
+  
